69be0028927a4a70d6c9a9a61fd9b24980a7b981
LexisCR <alejandrocortina1@gmail.com>
2025-10-29 09:29:39 -0500

    Deploy del contrato

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,11 +16,14 @@ const {ethers} = require("hardhat")
 
 async function multideploy(){
     const owners = ["0x59427DE366B815334d95267cE7968846Aa5Aa200","0x498a5083B953abD5bAb8136E0E4Fe2E22aeF42ba"]
-    const partes=["50","50"];
+    const partes=["60","40"];
     const requireApprovals = 2;
     const multisignWallet = await ethers.getContractFactory("MultiSignPaymentWallet");
     const wallet = await multisignWallet.deploy(owners, requireApprovals,owners,partes);
     console.log("WALLET_ADDRESS: ",wallet.address)
+    console.log("Direccion cuenta 1: ", owners[0])
+    console.log("Direccion cuenta 2: ", owners[1])
+    console.log("Gas utilizado: ",ethers.utils.formatEther(wallet.deployTransaction.gasPrice))
 }
 multideploy().then(()=>process.exit(0)).catch((error)=>{
     console.error(error);
